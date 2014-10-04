cf97fb99e6756050e4ea40472245f528616a89fa
Tom Scott <tscott@telvue.com>
2014-10-04 17:29:05 -0400

    Add app-wide 404 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery :with => :exception
 
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
-    attributes :edit_params
+  end
+
+  protected
+  def not_found
+    render 'errors/not_found', status: 404 and return
   end
 end

daa9aef893cea85d1e06138af5a83894ac2eaee4
Tom Scott <tscott@telvue.com>
2014-10-04 19:25:34 -0400

    Expand title_helper

--- app/helpers/title_helper.rb
+++ app/helpers/title_helper.rb
@@ -1,5 +1,31 @@
 module TitleHelper
   def title_tag
-    content_tag :title, "Wax Poetic Records"
+    content_tag :title, title_text
+  end
+
+  def title(new_title="")
+    content_for :title, new_title
+    content_tag :h1, new_title
+  end
+
+  private
+  def title_text
+    if page_has_title?
+      "#{page_title} | #{site_title}"
+    else
+      site_title
+    end
+  end
+
+  def site_title
+    "Wax Poetic Records"
+  end
+
+  def page_title
+    content_for :title
+  end
+
+  def page_has_title?
+    content_for? :title
   end
 end

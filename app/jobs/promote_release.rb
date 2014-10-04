676027d039e0972c8d56ed2f6e048c8e055d9cb0
Tom Scott <tscott@telvue.com>
2014-10-04 19:15:26 -0400

    Add tests for release promoter

--- app/jobs/promote_release.rb
+++ app/jobs/promote_release.rb
@@ -1,6 +1,6 @@
 class PromoteRelease < ActiveJob::Base
   def perform(release)
-    Rails.logger.warn "There were no emails sent" if Subscriber.all.empty?
+    logger.warn "There were no emails sent" if Subscriber.all.empty?
 
     Subscriber.all.each do |subscriber|
       PromoMailer.new_release(subscriber, release).deliver

de9a71b18d44e1f8eb345d62bab8253989e2e56e
Tom Scott <tscott@telvue.com>
2014-10-06 18:54:45 -0400

    Add better login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -614,12 +614,6 @@
 Rails.application.routes.draw do
   # User Authentication
   devise_for :users, skip: [:sessions]
-  devise_scope :user do
-    get    '/login'   => 'devise/sessions#new',     :as => :new_user_session
-    post   '/login'   => 'devise/sessions#create',  :as => :user_session
-    delete '/logout'  => 'devise/sessions#destroy', :as => :destroy_user_session
-    get    '/signup'  => 'devise/registrations#new'
-  end
 
   # Unauthenticated artist/release routes
   resources :artists

9c937f94ac0c9b26e90d806de8b6ffcad962b7dd
Tom Scott <tscott@telvue.com>
2014-10-06 13:44:03 -0400

    Add app/strategies

--- lib/decorated_strong_parameters_strategy.rb
+++ lib/decorated_strong_parameters_strategy.rb
@@ -1,13 +1,1 @@
-# A DecentExposure strategy that borrows from StrongParametersStrategy
-# but decorates all models before sending them to the view.
-class DecoratedStrongParametersStrategy < DecentExposure::StrongParametersStrategy
-  def resource
-    r = super.tap do |r|
-      unless r.respond_to?(:decorate)
-        r.class.send(:include, Draper::Decoratable)
-      end
-    end
 
-    r.decorate
-  end
-end

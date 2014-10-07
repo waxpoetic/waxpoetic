c3d02a6d8ca704856b40b99d728f914ef42540d8
Tom Scott <tscott@telvue.com>
2014-10-07 11:54:55 -0400

    Document global controller stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,23 @@
+# Global controller interface, methods here are active throughout the
+# application.
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery :with => :exception
 
+  # Return a 404 when a model can't be found.
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 
-  decent_configuration do
-    strategy DecoratedStrongParametersStrategy
-  end
+  # Configure DecentExposure to utilize StrongParameters and decorate
+  # all models it returns back.
+  decent_configuration { strategy DecoratedStrongParametersStrategy }
 
   protected
+  # The action rendered when a model can't be found.
   def not_found
     render 'errors/not_found', status: 404 and return
   end
 
+  # Formatted errors for the flash.
   def error_msg(model, message="could not be saved")
     "#{model.class.name.titleize} #{message}: #{model.errors.full_messages.join(', ')}"
   end

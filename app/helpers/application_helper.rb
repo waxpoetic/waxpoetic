36f4ded048c4ad27e8e13f0ff4b7b20c5700cde1
Tom Scott <tscott@telvue.com>
2014-10-12 21:58:31 -0400

    Group all Spree controllers under the same ID

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  SPREE_CONTROLLERS = %w(
+    products
+  )
+
   # Return the name of the CSS class for the given flash message type.
   def flash_class_for(flash_type)
     case flash_type
@@ -18,9 +22,17 @@ module ApplicationHelper
   # page. Marks the current controller.
   def page_tag_properties
     {
-      id: controller.controller_name,
+      id: page_id,
       class: 'page row'
     }
+  end
+
+  def page_id
+    if SPREE_CONTROLLERS.include?(controller.controller_name)
+      'store'
+    else
+      controller.controller_name
+    end
   end
 
   def gallery_for(resources)

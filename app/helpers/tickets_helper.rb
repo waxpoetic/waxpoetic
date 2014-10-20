b692c90f73a64a3ed85c67317a20db13ee444a61
Tom Scott <tscott@telvue.com>
2014-10-19 22:57:00 -0400

    Fix syntax

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,11 +1,11 @@
 module TicketsHelper
   # Link to the .pkpass for this ticket.
   def link_to_passbook(text, ticket)
-    link_to text, ticket_path(ticket, :format => :pdf), class: 'button' %>
+    link_to text, ticket_path(ticket, :format => :pdf), class: 'button'
   end
 
   # Link to the printable PDF for this ticket, includes the barcode.
   def link_to_pdf(text, ticket)
-    link_to text, ticket_path(ticket, :format => :pdf), class: 'button success' %>
+    link_to text, ticket_path(ticket, :format => :pdf), class: 'button success'
   end
 end

96741f2d24ecd81da3c9a173617eccee6ade94be
Tom Scott <tscott@telvue.com>
2014-11-30 19:34:27 -0500

    Annotate

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  caption    :string(255)
+#  file       :string(255)
+#  artist_id  :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_photos_on_artist_id  (artist_id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Photo, :type => :model do

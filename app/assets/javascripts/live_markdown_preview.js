8ea45fa0912fd4a72e26701a50ec1eac1fa9b319
Tom Scott <tscott@telvue.com>
2015-01-21 15:12:04 -0500

    Redocument manifest

--- app/assets/javascripts/live_markdown_preview.js
+++ app/assets/javascripts/live_markdown_preview.js
@@ -1,6 +1,4 @@
+// Manifest JS for the live_markdown_preview gem
+//
 //= require live_markdwon_preview/jquery.livepreview
 //= require live_markdown_preview/simple_form_integration
-
-$(document).ready(function() {
-  $('.preview.input').livePreview();
-});

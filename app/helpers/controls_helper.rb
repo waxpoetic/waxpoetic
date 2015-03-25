f59383afd61a3401a067dc6b783810729a6384b9
Tom Scott <tscott@weblinc.com>
2015-03-24 23:28:43 -0400

    Fix Ruby warning

--- app/helpers/controls_helper.rb
+++ app/helpers/controls_helper.rb
@@ -1,23 +1,23 @@
 module ControlsHelper
-  def resource_path(resource)
-    send "#{kind_of(resource)}_path", resource
+  def resource_path(model)
+    send "#{kind_of(model)}_path", model
   end
 
-  def edit_resource_path(resource)
-    send "edit_#{kind_of(resource)}_path", resource
+  def edit_resource_path(model)
+    send "edit_#{kind_of(model)}_path", model
   end
 
-  def resource_if_available
+  def and_resource_if_available
     try(:artist) || try(:release)
   end
 
-  def admin_controls(collection_name, resource: resource)
+  def admin_controls(collection_name, model)
     for_kind = collection_name.singularize
     case controller.action_name
     when 'index'
       new_item_button for_kind
     when 'show'
-      render 'controls', resource: resource, kind: for_kind
+      render 'controls', resource: for_resource, kind: for_kind
     end
   end
 
@@ -26,10 +26,8 @@ module ControlsHelper
     link_to "New #{kind}", route, class: 'button secondary'
   end
 
-
-
   private
-  def kind_of(resource)
-    resource.class.name.tableize.singularize
+  def kind_of(model)
+    model.class.name.tableize.singularize
   end
 end

78457f367a60993e84347eb6906733f008037dad
Tom Scott <tscott@weblinc.com>
2015-04-05 19:12:36 -0400

    Add mailchimp integration to EmailPromoter

--- app/promoters/email_promoter.rb
+++ app/promoters/email_promoter.rb
@@ -1,11 +1,22 @@
 # Send promotional emails to every Subscriber.
 class EmailPromoter < WaxPoetic::Promoter
-  def promote!(release, options={})
-    subscribers = options[:to]
-    logger.warn "There were no emails sent" unless subscribers.any?
+  RELEASE_LIST_ID = 12345
 
-    subscribers.each do |subscriber|
-      PromoMailer.new_release(subscriber, release).deliver
-    end
+  def promote!(release, options={})
+    mailchimp.campaigns.create(
+      type: 'regular',
+      options: {
+        list_id: RELEASE_LIST_ID,
+        subject: "#{release.name} by #{release.artist.name} has just been released!",
+        from_email: 'releases@waxpoeticrecords.com',
+        from_name: 'Wax Poetic Records',
+      }
+    )
+  end
+
+  private
+
+  def mailchimp
+    @mailchimp ||= Gibbon::API.new
   end
 end

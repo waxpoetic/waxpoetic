7d5772aa392350ac50746ca849d7c2c5ad3c1177
Tom Scott <tscott@weblinc.com>
2015-04-05 22:01:23 -0400

    Add preview generation to the MusicUploader

--- app/uploaders/music_uploader.rb
+++ app/uploaders/music_uploader.rb
@@ -1,5 +1,9 @@
 # For uploading music to Track or Podcast records.
 class MusicUploader < CarrierWave::Uploader::Base
+  version :preview do
+    process :trim
+  end
+
   # Always store track files in the track dir.
   def store_dir
     "tracks/#{model.id}/files"
@@ -8,5 +12,22 @@ class MusicUploader < CarrierWave::Uploader::Base
   # Only allow high-quality audio files to be uploaded.
   def extension_white_list
     %w(wav)
+  end
+
+  private
+
+  def trim
+    %x(
+      mv #{current_path} #{current_path}.old;
+      sox #{current_path}.old #{current_path} trim #{starts_at} #{ends_at};
+    )
+  end
+
+  def starts_at
+    model.preview_starts_at
+  end
+
+  def ends_at
+    model.preview_ends_at
   end
 end

49969b5f99c56b53e0083c8863d142314560a8d2
Tom Scott <tscott@weblinc.com>
2015-04-20 00:36:28 -0400

    Fix patch generator paths

--- lib/generators/patch/patch_generator.rb
+++ lib/generators/patch/patch_generator.rb
@@ -2,10 +2,10 @@ class PatchGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_patch
-    template 'patch.rb.erb', "app/patches/#{file_name}.rb"
+    template 'patch.rb.erb', "app/patches/#{file_path}.rb"
   end
 
   def copy_test
-    template 'spec.rb.erb', "spec/patches/#{file_name}_spec.rb"
+    template 'spec.rb.erb', "spec/patches/#{file_path}_spec.rb"
   end
 end

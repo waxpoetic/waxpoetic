9b02c055721b2459f9aff7bacb7524f43c1119d9
Tom Scott <tscott@weblinc.com>
2016-01-09 01:53:44 -0500

    Install Administrate

---
+++ app/dashboards/user_dashboard.rb
@@ -0,0 +1,51 @@
+require "administrate/base_dashboard"
+
+class UserDashboard < Administrate::BaseDashboard
+  READ_ONLY_ATTRIBUTES = [
+    :id,
+    :created_at,
+    :updated_at,
+  ]
+
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    email: Field::String,
+    encrypted_password: Field::String,
+    reset_password_token: Field::String,
+    reset_password_sent_at: Field::DateTime,
+    remember_created_at: Field::DateTime,
+    sign_in_count: Field::Number,
+    current_sign_in_at: Field::DateTime,
+    last_sign_in_at: Field::DateTime,
+    current_sign_in_ip: Field::String,
+    last_sign_in_ip: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    spree_api_key: Field::String,
+    ship_address_id: Field::Number,
+    bill_address_id: Field::Number,
+    name: Field::String,
+  }
+
+  # TABLE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to remove the limit or customize the returned array.
+  TABLE_ATTRIBUTES = ATTRIBUTE_TYPES.keys.first(4)
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = ATTRIBUTE_TYPES.keys
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = ATTRIBUTE_TYPES.keys - READ_ONLY_ATTRIBUTES
+end

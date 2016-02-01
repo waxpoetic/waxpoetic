5a2fc2c86f6b774da5ec02bb2642ee4d623a674f
Tom Scott <tscott@weblinc.com>
2016-01-31 22:00:01 -0500

    Add trix to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by showing a null session.
   protect_from_forgery with: :null_session
 
-  halt ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def not_found(exception)
     logger.error exception.message

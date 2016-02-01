61f4b063cd69149bc1d88592e3cdde85378466d4
Tom Scott <tscott@weblinc.com>
2016-01-31 21:42:54 -0500

    Configure DecentExposure to use collection decorators

--- app/controllers/application/exposure_strategy.rb
+++ app/controllers/application/exposure_strategy.rb
@@ -1,9 +1,17 @@
 module Application
+  # Configure +DecentExposure+ to decorate collections and singular
+  # resources.
   class ExposureStrategy < DecentExposure::StrongParametersStrategy
+    # Return a decorated collection resource.
+    #
+    # @return [Draper::CollectionDecorator]
     def collection_resource
-      collection_decorator.new super
+      super.decorate_collection
     end
 
+    # Return a singular decorated model resource.
+    #
+    # @return [Draper::Decorator]
     def singular_resource
       super.decorate
     end

c403f2270a912638ccd5a6b821e8903b46e7999d
Tom Scott <tscott@weblinc.com>
2016-02-06 14:34:22 -0500

    Update Rails to secure version and update TODO

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,15 +5,11 @@
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 class Admin::ApplicationController < Administrate::ApplicationController
-  before_action :authenticate_admin
+  before_action :authenticate_user!
 
-  def authenticate_admin
-    # TODO Add authentication logic here.
+  # Override this value to specify the number of elements
+  # to display at a time on index pages. Defaults to 20.
+  def records_per_page
+    params[:per_page] || 20
   end
-
-  # Override this value to specify the number of elements to display at a time
-  # on index pages. Defaults to 20.
-  # def records_per_page
-  #   params[:per_page] || 20
-  # end
 end
